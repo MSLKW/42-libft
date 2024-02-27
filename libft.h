786608c679d2ed773af39b4b417f01117c8f09b0
Maxwell Liew <maxliew@u82z04s04.42kl.edu.my>
2024-02-27 08:52:31 +0800

    Fixed libft.h, have not implemented memory prototypes yet

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: maxliew <maxliew@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 10:54:28 by maxliew           #+#    #+#             */
-/*   Updated: 2024/02/27 08:47:09 by maxliew          ###   ########.fr       */
+/*   Updated: 2024/02/27 08:52:09 by maxliew          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 # define CAPITALIZATION_DIFF 32
+
+// #ifndef NULL
+// # define NULL ((void*)0)
+// #endif
+// typedef unsigned long size_t;
 
 int	ft_strlen(char *str);
 int	ft_isalpha(int c);
@@ -29,6 +34,7 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strrchr(const char *str, int c);
-
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
 
 #endif
